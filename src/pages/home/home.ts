2b9af60bc46c5383b1123fccabdb9213136e77b1
alex marcos <alejandro.marcos@tmtfactory.com>
2017-08-09 01:40:38 +0200

    [home] -> Preparar home con transparent navbar

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {Content} from "ionic-angular";
 
 
 @Component({
@@ -6,9 +7,12 @@ import { Component } from '@angular/core';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  @ViewChild(Content) content: Content;
   constructor() {
-
+    console.log(this.content)
   }
 
+  ionViewWillEnter() { // THERE IT IS!!!
+    this.content._elementRef.nativeElement.childNodes[1].classList.add("no-header")
+  }
 }
